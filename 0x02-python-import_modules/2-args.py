334e63dae73753a472aa22ad03ca43f2abcb5626
TicklemeKrystle <krystle.akuffo@gmail.com>
2022-06-03 01:39:08 -0700

    Tasks

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-from add_0 import add
+import sys
 
 if __name__ == "__main__":
-    a = 1
-    b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+    i = len(sys.argv) - 1
+    if i == 0:
+        print("0 arguments.")
+    elif i == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(i))
+
+    for n in range(i):
+        print("{}: {}".format(n + 1, sys.argv[n + 1]))
